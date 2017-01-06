a518c2c8ac5982cc03e6e7068f9bed9a8ba015c2
DonnieTu <tud@Donnies-MacBook-Air.local>
2017-01-06 19:34:27 +0800

    call fibonacci service from web server

--- fibonacci/routes/fibonacci.js
+++ fibonacci/routes/fibonacci.js
@@ -2,17 +2,31 @@
 var express = require('express');
 var router = express.Router();
 var math=require('../math');
+var http=require('http');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if(req.query.fibonum) {
-      math.fibonacci(req.query.fibonum,(err,val)=>{
-            res.render('fibonacci', { 
+    var httpreq= http.request({
+      host:'localhost',
+      port:'3002',
+      method:'GET',
+      path: '/fibonacci/'+req.query.fibonum
+    },httpres=>{
+      httpres.on('data',chunk=>{
+        res.render('fibonacci', { 
               title: 'Math Calculator',
               fibonum:req.query.fibonum,
-              fiboval:val
+              fiboval:JSON.parse(chunk).result
             });
-        });
+      });
+      httpres.on('error',err=>{
+        console.log(err.message);
+        next(err);
+      });
+    });
+    httpreq.on('error',err=>{next(err);});
+    httpreq.end();
   } else {
     res.render('fibonacci', { 
       title: 'Math Calculator',
