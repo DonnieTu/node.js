f41153be43e986b8b69208141fdac58d242a26f4
DonnieTu <donnie.tu@autodesk.com>
2017-02-16 22:20:28 +0800

    add function to users-sequelize.js

--- users/users-sequelize.js
+++ users/users-sequelize.js
@@ -39,3 +39,109 @@ exports.connectDB=function() {
         return SQUser.sync();
     });
 };
+
+exports.create=function(username, password, provider, familyName, givenName, middleName, emails, photos) {
+    return exports.connectDB()
+    .then(SQUser=>{
+        return SQUser.create({
+            username: username,
+            password: password,
+            provider: provider,
+            familyName: familyName,
+            givenName: givenName,
+            middleName: middleName,
+            emails: JSON.stringify(emails),
+            photos: JSON.stringify(photos)
+        });
+    });
+};
+
+exports.update=function(username, password, provider, familyName, givenName, middleName, emails, photos) {
+    return exports.connectDB()
+    .then(SQUser=>{
+        return SQUser.findOne({where: {username:username}});
+    })
+    .then(user=>{
+        return user?user.update({
+            password: password,
+            provider: provider,
+            familyName: familyName,
+            givenName: givenName,
+            middleName: middleName,
+            emails: JSON.stringify(emails),
+            photos: JSON.stringify(photos)
+        }):undefined;
+    });
+};
+
+exports.find=function(username) {
+    log('find '+username);
+    return exports.connectDB()
+    .then(SQUser=>{
+        return SQUser.findOne({where: {username:username}});
+    })
+    .then(user=>{
+        user?exports.sanitizedUser(user):undefined;
+    });
+};
+
+exports.destory=function(username) {
+    return exports.connectDB()
+    .then(SQUser=>{
+        return SQUser.findOne({where:{username:username}});
+    })
+    .then(user=>{
+        if(!user) throw new Error ("Did not find requested "+username+ ' to delete');
+        user.destory();
+        return;
+    });
+};
+
+exports.userPasswordCheck=function(username,password) {
+    return exports.connectDB().then(SQUser=>{
+        return SQUser.findOne({where:{username:username}});
+    })
+    .then(user=>{
+        if(!user) {
+            return {check:false, username:username,message:"Could not find user"};
+        } else if(user.username==username && user.password==password) {
+            return {check:true,username:user.username};
+        } else {
+            return {check:false,username:username,message:"Incorrect password"};
+        }
+    });
+};
+
+exports.findOrCreate=function(profile) {
+    return exports.find(profile.id)
+    .then(user=>{
+        if(user) return user;
+        return exports.create(profile.id, profile.password,
+            profile.provider, profile.familyName,
+            profile.givenName, profile.middleName,
+            profile.emails, profile.photos);
+    });
+};
+
+exports.sanitizedUser = function(user) {
+    return {
+        id: user.username,
+        username: user.username,
+        provider: user.provider,
+        familyName: user.familyName,
+        givenName: user.givenName,
+        middleName: user.middleName,
+        emails: user.emails,
+        photos: user.photos
+    };
+};
+
+exports.listUsers=function(){
+    exports.connectDB()
+    .then(SQUser=>SQUser.findAll({}))
+    .then(users=>users.map(
+        user=>exports.sanitizedUser(user))
+    )
+    .catch(err=>error(err));
+};
+
