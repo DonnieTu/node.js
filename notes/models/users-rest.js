30ed12202c0f7351b8d01f16454ae9a5b363a8cc
DonnieTu <donnie.tu@autodesk.com>
2017-03-07 07:32:18 +0800

    add more funtion for user-rest.js

--- notes/models/users-rest.js
+++ notes/models/users-rest.js
@@ -6,6 +6,8 @@ const log=require('debug')('notes:users-rest-client');
 const error=require("debug")('notes:error');
 
 var connectREST=function() {
+
+
     return new Promise((resolve,reject)=>{
         try {
             reslove(restify.createJsonClient({
@@ -33,4 +35,30 @@ exports.create=function(username, password,provider,familyName,givenName,middleN
                 });
             });
         });
+};
+
+exports.update=function(username, password,provider,familyName,givenName,middleName,emails,photos) {
+    return connectREST().then(client=>{
+        return new Promise((resolve,reject)=>{
+            client.post('/update-user'+username,{
+                password,provider,familyName,givenName,middleName,emails,photos
+            },
+            (err,req,res,obj)=>{
+                if(err) return reject(err);
+                resolve(obj);
+            });
+        });
+    });
+};
+
+exports.find=function(username) {
+    return connectREST().then(client=>{
+        return new Promise((resolve,reject)=>{
+            client.get('/find/'+username,
+                (err,req,res,obj)=>{
+                    if(err) return reject(err);
+                    resolve(obj);
+                });
+        });
+    });
 };
