f7524c2714a4887e6829cab461415e3ed0d9c069
DonnieTu <donnie.tu@autodesk.com>
2017-03-16 21:54:18 +0800

    use twitter

--- notes/routes/users.js
+++ notes/routes/users.js
@@ -8,6 +8,7 @@ const router = express.Router();
 exports.router=router;
 const passport=require('passport');
 const LocalStrategy=require('passport-local').Strategy;
+const TwitterStrategy=require('passport-twitter').Strategy;
 const usersModel=require(process.env.USERS_MODEL?path.join('..',process.env.USERS_MODEL)
   :'../models/users-rest');
 
@@ -43,6 +44,11 @@ router.get('/logout',function(req,res,next){
   res.redirect('/');
 });
 
+router.get('/auth/twitter',passport.authenticate('twitter'));
+router.get('/auth/twitter/callback',
+  passport.authnticate('twitter',{successRedirect:'/',
+    failureRedirect:'/users/login'}));
+
 
 passport.use(new LocalStrategy((username,password,done)=>{
     usersModel.userPasswordCheck(username,password)
@@ -61,6 +67,27 @@ passport.use(new LocalStrategy((username,password,done)=>{
     });
   })
 );
+
+passport.use(new TwitterStrategy({
+  consumerKey:	"K5eeZs9onMyPyNApQJXUwnAYu",
+  consumerSecret: "RkjyH61qtXRDh5XpzxSrLSGon6d09I49ySeBLSCyFKeppOHRBQ",
+  callbackURL:"	http://10.148.185.17:3000/users/auth/twitter/callback"
+},
+(token,tokenSecret,profile,done)=>{
+  usersModel.findOrCreate({
+    id:profile.username,
+    username:profile.username,
+    password:"",
+    provider:profile.provider,
+    familyName:profile.displayName,
+    givenName:"",
+    middleName:"",
+    photos:profile.photos,
+    emails:profile.emails
+  })
+  .then(user=>done(null,user))
+  .catch(err=>done(err));
+}));
 
 passport.serializeUser((user,done)=>{
   done(null,user.username);
