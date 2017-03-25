0a83446a963bda9ff49d93fee5599cb3d5d17f32
DonnieTu <tud@Donnies-MacBook-Air.local>
2017-03-25 13:09:25 +0800

    fix testing

--- Chapter 9/notes/test/test-model.js
+++ Chapter 9/notes/test/test-model.js
@@ -2,11 +2,12 @@
 const assert=require('chai').assert;
 const model=require(process.env.MODEL_TO_TEST);
 
+
 describe("Model Test", function(){
     beforeEach(function(){
         return model.keylist()
         .then(keys=>{
-            var todel=keys.map(key=>model.destory(key));
+            var todel=keys.map(key=>model.destroy(key));
             return Promise.all(todel);
         })
         .then(()=>{
@@ -24,32 +25,32 @@ describe("Model Test", function(){
             return model.keylist()
             .then(keys=>{
                 assert.equal(3,keys.length,"length 3");
+            });
+        });
+
+
+        it("should have keys n1 n2 n3",function(){
+            return model.keylist()
+            .then(keys=>{
+                keys.forEach((key)=>{
+                    assert.match(key,/n[123]/,"correct key");
+                });
+            });
+        });
+
+        it("Should have titles Node #",function(){
+            return model.keylist()
+            .then(keys=>{
+                var keyPromises=keys.map(key=>{
+                    return model.read(key);
+                });
+                return Promise.all(keyPromises);
             })
-        });
-    });
-
-    describe("should have keys n1 n2 n3",function(){
-        return model.keylist()
-        .then(keys=>{
-            keys.forEach((key)=>{
-                assert.match(key,/n[123]/,"correct key");
+            .then(notes=>{
+                notes.forEach(note=>{
+                    assert.match(note.title,/Note [123]/);
+                });
             });
-        });
+        });     
     });
-
-    describe("Should have titles Node #",function(){
-        return model.keylist()
-        .then(keys=>{
-            var keyPromises=keys.map(key=>{
-                model.read(key);
-            });
-            Promises.all(keyPromises);
-        })
-        .then(notes=>{
-            notes.forEach(note=>{
-                assert.match(note.title,/Note [123]/);
-            });
-        });
-    });
-
 });
