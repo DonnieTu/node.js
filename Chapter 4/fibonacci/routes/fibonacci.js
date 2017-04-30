6c62552c43140d948d6cf8d671a45d943e88987d
DonnieTu <donnie.tu@autodesk.com>
2017-04-30 22:18:45 +0800

    use restify to call fibonacci service

--- Chapter 4/fibonacci/routes/fibonacci.js
+++ Chapter 4/fibonacci/routes/fibonacci.js
@@ -1,16 +1,20 @@
 var express=require('express');
+var restify = require('restify');
 var router=express.Router();
 
 var math=require('../math');
 router.get('/',function(req,res,next){
     if(req.query.fibonum) {
+        //use fibonacciAsync directly
         /*math.fibonacciAsync(req.query.fibonum,(err,val)=>{
             res.render('fibonacci',{
                 title:"Calculate Fibonacci numbers",
                 fibonum: req.query.fibonum,
                 fiboval: val});
         });*/
-        var httpreq=require('http').request({
+
+        //use Fibonaservice
+        /*var httpreq=require('http').request({
             hostname:"localhost",
             port:3002,
             path:'/fibonacci/'+Math.floor(req.query.fibonum),
@@ -27,7 +31,18 @@ router.get('/',function(req,res,next){
             httpres.on('error',err=>{next(err);});
         });
         httpreq.on('error',err=>next(err));
-        httpreq.end();
+        httpreq.end();*/
+
+        // use restify to call fibona service
+        var client = restify.createJsonClient({
+            url: 'http://localhost:3002'});
+
+        client.get('/fibonacci/'+Math.floor(req.query.fibonum), function(err, httpreq, httpres, result) {
+            res.render('fibonacci',{
+                title:"Calculate Fibonacci numbers",
+                fibonum: req.query.fibonum,
+                fiboval: result.result});
+        });
     } else {
         res.render('fibonacci',{
             title: "Calculate Fibonacci numbers",
