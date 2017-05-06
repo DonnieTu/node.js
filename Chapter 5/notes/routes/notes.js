e408c0edb4786077421559876c2307ba77ba35de
DonnieTu <donnie.tu@autodesk.com>
2017-05-06 22:23:04 +0800

    notes/view

---
+++ Chapter 5/notes/routes/notes.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+var notes=require('../models/notes-memory');
+
+/* GET users listing. */
+router.get('/add', function(req, res, next) {
+
+  res.render('noteedit',{
+      title:"Add a Note",
+      docreate:true,
+      notekey:"",
+      note:undefined
+  });
+});
+
+router.post('/save',function(req,res,next) {
+    var p;
+    if(req.body.docreate=='create') {
+        p=notes.create(req.body.notekey,req.body.title,req.body.body);
+    } else {
+        p=notes.update(req.body.notekey,req.body.title,req.body.body);
+    }
+
+    p.then(note=>{
+        res.redirect('/notes/view?key='+req.body.notekey);
+    })
+    .catch(err=>{
+        next(err);
+    });
+});
+
+router.get('/view',function(req,res,next){
+    notes.read(req.query.key)
+    .then(note=>{
+        res.render('noteview',{
+            title:note?note.title:'',
+            notekey:req.query.key,
+            note:note
+        });
+    })
+    .catch(err=>next(err));
+});
+
+module.exports = router;
